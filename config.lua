d7ad77acd10ca540ee2ba9a734e8edeacda0dc13
philmcracken892 <MARC.KELLETT@GOOGLEMAIL.COM>
2025-07-23 16:28:35 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -11,12 +11,12 @@ Config.ServiceExtensionOnEscape = 5
 
 -- Community service work locations
 Config.ServiceLocations = {
-    {coords = vector3(-309.25, 800.91, 118.98), type = "cleaning"},
-    {coords = vector3(-306.10, 805.73, 118.98), type = "cleaning"},
-    {coords = vector3(-311.64, 806.19, 118.98), type = "maintenance"},
-    {coords = vector3(-302.00, 781.90, 118.75), type = "cleaning"},
-    {coords = vector3(-341.08, 767.38, 116.70), type = "maintenance"},
-    {coords = vector3(-374.66, 740.16, 115.88), type = "cleaning"},
-    {coords = vector3(-283.06, 785.02, 119.47), type = "maintenance"},
-    {coords = vector3(-232.20, 794.89, 124.62), type = "cleaning"},
+    {coords = vector3(-295.89, 800.70, 119.29), type = "cleaning"},
+    {coords = vector3(-291.53, 784.37, 119.28), type = "cleaning"},
+    {coords = vector3(-280.95, 784.91, 119.47), type = "maintenance"},
+    {coords = vector3(-236.41, 804.40, 123.51), type = "cleaning"},
+    {coords = vector3(-244.93, 766.68, 118.22), type = "maintenance"},
+    {coords = vector3(-255.49, 741.48, 118.17), type = "cleaning"},
+    {coords = vector3(-325.33, 777.34, 117.42), type = "maintenance"},
+    {coords = vector3(-360.88, 781.97, 116.19), type = "cleaning"},
 }
